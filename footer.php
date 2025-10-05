8de4195d4d99d2d12c5a696db49e6fd2cb1fdfdf
Milan Zidek <xmilanz@gmail.com>
2025-10-05 14:11:04 +0200

    fix footer text position on low resolutions

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 </div>
 </div>
-<div id="footer">&copy; Jiří Šedina 2010 & Jan JUMBO Staněk 2018 & Milan Žídek <?= date("Y") ?> <span style="float:right">Shooting match registration system ver. 1.0</span></div>
+<div id="footer">&copy; Jiří Šedina 2010 & Jan JUMBO Staněk 2018 & Milan Žídek <?= date("Y") ?> <p class="version">Shooting match registration system ver. 1.0</p></div>
 </body>
 </html>
